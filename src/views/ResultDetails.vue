eb0e0eea1086c10f89e83532d3cbd77798770570
Edward <edward.olmos.570@my.csun.edu>
2020-10-02 23:43:32 -0700

    got basic vue running .. still need to clean up

--- src/views/ResultDetails.vue
+++ src/views/ResultDetails.vue
@@ -3,10 +3,13 @@
     <h2 style="text-align: center;">
       Comparison: {{ this.$route.params.slug }} (score: {{ result.score }})
     </h2>
+    <div v-if="hover">Show</div>
     <table>
       <thead>
         <tr>
-          <th colspan="1">{{ result.fileOneName }}</th>
+          <th colspan="1">
+            {{ result.fileOneName }}
+          </th>
           <th colspan="1">{{ result.fileTwoName }}</th>
         </tr>
       </thead>
@@ -15,9 +18,14 @@
           <td>
             <div id="a">
               <div
+                @mouseover="mouseOver"
+                @mouseleave="mouseLeave"
                 v-for="(line, index) in result.fileOneLines"
                 :key="index"
-                :class="isMatch ? 'matched-theme' : 'normal-theme'"
+                :class="[
+                  isMatch(index) ? 'matched-theme' : 'normal-theme',
+                  {'hovered-theme': hovered}
+                ]"
               >
                 {{ index + 1 }} | {{ line }}
               </div>
@@ -25,7 +33,11 @@
           </td>
           <td>
             <div id="b">
-              <div v-for="(line, index) in result.fileTwoLines" :key="index">
+              <div
+                v-for="(line, index) in result.fileTwoLines"
+                :key="index"
+                :class="isMatch(index) ? 'matched-theme' : 'normal-theme'"
+              >
                 {{ index + 1 }} | {{ line }}
               </div>
             </div>
@@ -41,7 +53,10 @@ import store from "@/store.js";
 
 export default {
   data() {
-    return {};
+    return {
+      hovered: false,
+      active: true
+    };
   },
   props: {
     slug: {
@@ -53,16 +68,22 @@ export default {
   computed: {
     result() {
       return store.results.find(result => result.slug === this.slug);
-    },
+    }
+  },
+  methods: {
     isMatch: function(num) {
       var temp = store.results.find(result => result.fileOneMatches);
-      var temp2 = num;
-      console.log("Hello " + temp2);
-      if (temp.fileOneMatches.indexOf(5) !== -1) {
+      if (temp.fileOneMatches.indexOf(num) !== -1) {
         return true;
       } else {
         return false;
       }
+    },
+    mouseOver: function() {
+      this.hovered = !this.hovered;
+    },
+    mouseLeave: function() {
+      this.hovered = !this.hovered;
     }
   }
 };
@@ -80,6 +101,7 @@ th {
   border: 1px solid #333;
   justify-content: center;
   padding: 20px;
+  vertical-align: super;
 }
 
 thead,
@@ -96,7 +118,13 @@ tfoot {
 }
 
 .normal-theme {
-  height: 90vh;
+  width: 40vw;
+  margin-left: auto;
+  font-size: 1.2em;
+}
+
+.hovered-theme {
+  color: green;
   width: 40vw;
   margin-left: auto;
   font-size: 1.2em;
